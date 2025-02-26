53ec1f996deb7e98c8a3fd92a14a2ef986b812a5
9Sefa9 <sefagoevercin@hotmail.de>
2025-02-27 00:34:06 +0100

    1 - fixed #1 o auth signout button

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -18,9 +18,13 @@ const Navbar = async () => {
                             <Link href="/startup/create">
                                 <span>Create</span>
                             </Link>
-                            <button onClick={async () => await signOut()}>
-                                <span>Logout</span>
-                            </button>
+                            <form action={async () => {
+                                'use server';
+
+                                await signOut({ redirectTo: '/' })
+                            }}>
+                                <button type="submit">Logout</button>
+                            </form>
                             <Link href={'/user/${session?.id}'}>
                                 <span>{session?.user?.name}</span>
                             </Link>
